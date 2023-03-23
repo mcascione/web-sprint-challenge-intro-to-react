040f3cc1ab4b4a7ce3daf50d884f0375f39cf8c5
Miranda Cascione <miranda.cascione@gmail.com>
2023-03-22 20:01:55 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from "./components/Character";
+import "./App.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const[data, setData] = useState();
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(()=>{
+    axios.get("https://swapi.dev/api/people/")
+    .then(res => {
+      setData(res.data);
+    })
+    .catch(err => console.error(err));
+
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data && <Character data={data}/>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    margin: 0 20% 0 20%;
+    justify-content: space-between;
+    color: orange;
+`;
+
+const Character = (props) => {
+    const data = props.data;
+    const characterNames = data.map((el) => {
+        return (el.name);
+    })
+    const characterName = characterNames.map(element => {
+        return element;
+    });
+
+    return (
+        <CharacterWrapper>
+            <h2>{characterName[0]}</h2> 
+            <h2>{characterName[1]}</h2> 
+            <h2>{characterName[2]}</h2> 
+            <h2>{characterName[3]}</h2> 
+            <h2>{characterName[4]}</h2> 
+            <h2>{characterName[5]}</h2>      
+        </CharacterWrapper>
+    );
+}
+
+export default Character;
